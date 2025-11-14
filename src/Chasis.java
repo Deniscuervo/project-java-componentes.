abcecd3e51a574a412be72e3c84103bc44c038db
Denis Cuervo <dlisethdaza@gmail.com>
2025-11-14 11:16:16 -0500

    implementación de clases Chasis y Neumatico

--- src/Chasis.java
+++ src/Chasis.java
@@ -1,3 +1,19 @@
 public class Chasis {
-    
+
+    private String tipo;
+    private double largo;
+    private double peso;
+
+    public Chasis(String tipo, double largo, double peso) {
+        this.tipo = tipo;
+        this.largo = largo;
+        this.peso = peso;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Chasis:");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Largo: " + largo + " m");
+        System.out.println("Peso: " + peso + " kg");
+    }
 }
--- src/Neumatico.java
+++ src/Neumatico.java
@@ -1,3 +1,19 @@
 public class Neumatico {
-    
+
+    private String marca;
+    private int tamaño;
+    private double presion;
+
+    public Neumatico(String marca, int tamaño, double presion) {
+        this.marca = marca;
+        this.tamaño = tamaño;
+        this.presion = presion;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Neumático:");
+        System.out.println("Marca: " + marca);
+        System.out.println("Tamaño: " + tamaño);
+        System.out.println("Presión: " + presion + " PSI");
+    }
 }
