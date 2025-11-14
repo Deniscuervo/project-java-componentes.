5f0e25996b1376ba0137b16b9cd7d8f6c48e6bc6
Denis Cuervo <dlisethdaza@gmail.com>
2025-11-14 11:18:41 -0500

    implementación de clases SistemaElectrico y SistemaFrenos

--- src/SistemaElectrico.java
+++ src/SistemaElectrico.java
@@ -1,3 +1,19 @@
 public class SistemaElectrico {
-    
+
+    private int capacidadBateria;
+    private boolean lucesLed;
+    private int sensores;
+
+    public SistemaElectrico(int capacidadBateria, boolean lucesLed, int sensores) {
+        this.capacidadBateria = capacidadBateria;
+        this.lucesLed = lucesLed;
+        this.sensores = sensores;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Sistema Eléctrico:");
+        System.out.println("Capacidad bateria: " + capacidadBateria + " Ah");
+        System.out.println("Luces LED: " + lucesLed);
+        System.out.println("Cantidad sensores: " + sensores);
+    }
 }
--- src/SistemaFrenos.java
+++ src/SistemaFrenos.java
@@ -1,3 +1,19 @@
 public class SistemaFrenos {
-    
+
+    private String tipo;
+    private boolean abs;
+    private int discos;
+
+    public SistemaFrenos(String tipo, boolean abs, int discos) {
+        this.tipo = tipo;
+        this.abs = abs;
+        this.discos = discos;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Sistema de Frenos:");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("ABS: " + abs);
+        System.out.println("Discos: " + discos);
+    }
 }
